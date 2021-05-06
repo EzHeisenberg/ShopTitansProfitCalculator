88f8fe52014319068112bd91f1d1ffd8879a1cc0
EzHeisenberg <lucaporal@live.fr>
2021-05-07 01:18:05 +0200

    deploy

---
+++ precache-manifest.9b1e93d8d3bb27924150bd5aa2b8a768.js
@@ -0,0 +1,258 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f33f456a7f6b23b7cf27",
+    "url": "/ShopTitansProfitCalculator/css/app.5d7d4698.css"
+  },
+  {
+    "revision": "814d40aa4d5fd1b4c348",
+    "url": "/ShopTitansProfitCalculator/css/chunk-vendors.41cb8091.css"
+  },
+  {
+    "revision": "47f711bd1524b9283aa92d3d74db16a8",
+    "url": "/ShopTitansProfitCalculator/fonts/BebasNeue-Regular.47f711bd.ttf"
+  },
+  {
+    "revision": "5d3c4a34c8744610e421024f21b2cea5",
+    "url": "/ShopTitansProfitCalculator/fonts/EurostyleB.5d3c4a34.ttf"
+  },
+  {
+    "revision": "7e1390fd175af6ecbfe7ba5789e4c642",
+    "url": "/ShopTitansProfitCalculator/fonts/Pr.7e1390fd.ttf"
+  },
+  {
+    "revision": "6dd7239bfe311b12fb84c4283a878ca9",
+    "url": "/ShopTitansProfitCalculator/fonts/mk2.6dd7239b.ttf"
+  },
+  {
+    "revision": "0b77d37387257dc67b5d2f1c7178ca6d",
+    "url": "/ShopTitansProfitCalculator/img/Ancient Marble.0b77d373.png"
+  },
+  {
+    "revision": "c06b3f66ff7661f237f6c1b91419e73a",
+    "url": "/ShopTitansProfitCalculator/img/Boreal Gale.c06b3f66.png"
+  },
+  {
+    "revision": "483d58bfcb4c04eda3c6a0105f95d26b",
+    "url": "/ShopTitansProfitCalculator/img/Bronze Fang.483d58bf.png"
+  },
+  {
+    "revision": "e54128b1d5b8c5c29db069e67d816eea",
+    "url": "/ShopTitansProfitCalculator/img/Chipped Runestone.e54128b1.png"
+  },
+  {
+    "revision": "32484c268fabea90f3f426e828c85b64",
+    "url": "/ShopTitansProfitCalculator/img/Chiseled Runestone.32484c26.png"
+  },
+  {
+    "revision": "359e75caec03fc8abfbbc236151fab81",
+    "url": "/ShopTitansProfitCalculator/img/Chronos Crystal.359e75ca.png"
+  },
+  {
+    "revision": "56827b3a5a2858b456f694d7dff31b9f",
+    "url": "/ShopTitansProfitCalculator/img/Crystal Lullaby.56827b3a.png"
+  },
+  {
+    "revision": "907a3efea5114940e971e7714dc772f8",
+    "url": "/ShopTitansProfitCalculator/img/Deep Coral.907a3efe.png"
+  },
+  {
+    "revision": "8d3910caed50f3d7635647061ea161fb",
+    "url": "/ShopTitansProfitCalculator/img/Deep Pearl.8d3910ca.png"
+  },
+  {
+    "revision": "76d98aa328a79cbdba451725551ba90a",
+    "url": "/ShopTitansProfitCalculator/img/Elven Wood.76d98aa3.png"
+  },
+  {
+    "revision": "3b33857c4266ecb83e64dd520359a11c",
+    "url": "/ShopTitansProfitCalculator/img/Evil Eye.3b33857c.png"
+  },
+  {
+    "revision": "f1be976f8b6176c96f581713aded56a2",
+    "url": "/ShopTitansProfitCalculator/img/Flawed Runestone.f1be976f.png"
+  },
+  {
+    "revision": "6b2474ed19ec0e74135cc9a895f82bb5",
+    "url": "/ShopTitansProfitCalculator/img/Ghastly Pennant.6b2474ed.png"
+  },
+  {
+    "revision": "739f3eecf38924a5a0266e8085a1b938",
+    "url": "/ShopTitansProfitCalculator/img/Glow Shroom.739f3eec.png"
+  },
+  {
+    "revision": "eb95ab13e882aca9e1d4bd92f1d9a2fb",
+    "url": "/ShopTitansProfitCalculator/img/Golden Artifact.eb95ab13.png"
+  },
+  {
+    "revision": "314dc39ef53ae3cdbb89ce1d2cda2550",
+    "url": "/ShopTitansProfitCalculator/img/Grim Talon.314dc39e.png"
+  },
+  {
+    "revision": "033b8f0ca4e5c147c68ca16e26edd707",
+    "url": "/ShopTitansProfitCalculator/img/Iron Pine Cone.033b8f0c.png"
+  },
+  {
+    "revision": "0ee42223bbb6878577212ccd56e7fbe5",
+    "url": "/ShopTitansProfitCalculator/img/Living Root.0ee42223.png"
+  },
+  {
+    "revision": "513a8bed881370f29c12600b484c3b35",
+    "url": "/ShopTitansProfitCalculator/img/Moon Crystal.513a8bed.png"
+  },
+  {
+    "revision": "550ceced4e3d6787c6939eb402a91dc2",
+    "url": "/ShopTitansProfitCalculator/img/Opulent Jewel.550ceced.png"
+  },
+  {
+    "revision": "68ea28fc4645ed39dfebf03e33c40ff0",
+    "url": "/ShopTitansProfitCalculator/img/Overgrown Vine.68ea28fc.png"
+  },
+  {
+    "revision": "ce592e5851fda1fcc13a54e24e0372d4",
+    "url": "/ShopTitansProfitCalculator/img/Precious Gem.ce592e58.png"
+  },
+  {
+    "revision": "40903042c410bd9429ba40b0bb5179ef",
+    "url": "/ShopTitansProfitCalculator/img/Precious Shell.40903042.png"
+  },
+  {
+    "revision": "98429f4c64afccbdc885ca5159d3ce71",
+    "url": "/ShopTitansProfitCalculator/img/Rustwyrm Scale.98429f4c.png"
+  },
+  {
+    "revision": "5c099c87c816f1284f4919a28d3e2dfe",
+    "url": "/ShopTitansProfitCalculator/img/Sigil of Grace.5c099c87.png"
+  },
+  {
+    "revision": "01a3d4e9e68ead3c3ac323514c4a4bfa",
+    "url": "/ShopTitansProfitCalculator/img/Sigil of Might.01a3d4e9.png"
+  },
+  {
+    "revision": "a269d50b50ed5834a3a2697520b518e8",
+    "url": "/ShopTitansProfitCalculator/img/Sigil of Spark.a269d50b.png"
+  },
+  {
+    "revision": "72c0123da670d62270a8498182e76d79",
+    "url": "/ShopTitansProfitCalculator/img/Silk Scarab.72c0123d.png"
+  },
+  {
+    "revision": "3a032b49124ae309f8dd42af4b93e51d",
+    "url": "/ShopTitansProfitCalculator/img/Silver Dust.3a032b49.png"
+  },
+  {
+    "revision": "a2bd6aa6aa88fc9ccfb8af6caffa8f0c",
+    "url": "/ShopTitansProfitCalculator/img/Spooky Ectoplasm.a2bd6aa6.png"
+  },
+  {
+    "revision": "283432b76485722bbb5a655596716637",
+    "url": "/ShopTitansProfitCalculator/img/Star Metal.283432b7.png"
+  },
+  {
+    "revision": "c51ec79992f99c776b9034e209d41607",
+    "url": "/ShopTitansProfitCalculator/img/Thread of Fate.c51ec799.png"
+  },
+  {
+    "revision": "974c9a6d8f1c20461fd5bea9b2fa244f",
+    "url": "/ShopTitansProfitCalculator/img/Webbed Wing.974c9a6d.png"
+  },
+  {
+    "revision": "ce05eed763a543316de6c7527258aa3d",
+    "url": "/ShopTitansProfitCalculator/img/White Sand.ce05eed7.png"
+  },
+  {
+    "revision": "2d437f59ddbbf8a4509fae92561239df",
+    "url": "/ShopTitansProfitCalculator/img/Zirconia Eggshell.2d437f59.png"
+  },
+  {
+    "revision": "92ef2c3cad7d23024a8fb05ec4e1eaac",
+    "url": "/ShopTitansProfitCalculator/img/ether.92ef2c3c.png"
+  },
+  {
+    "revision": "e727305b98d85e0cfe47bef6f19912d6",
+    "url": "/ShopTitansProfitCalculator/img/fabric.e727305b.png"
+  },
+  {
+    "revision": "173f5324342804bc1a90beb0ae4506a4",
+    "url": "/ShopTitansProfitCalculator/img/favicon-night.173f5324.png"
+  },
+  {
+    "revision": "7198fd6a64da108f9ea358e2d7ecd0f5",
+    "url": "/ShopTitansProfitCalculator/img/github.7198fd6a.png"
+  },
+  {
+    "revision": "42e299b2594db41704924bec0e986cce",
+    "url": "/ShopTitansProfitCalculator/img/gold.42e299b2.png"
+  },
+  {
+    "revision": "9d821e973415d362813c1a11c7c012e0",
+    "url": "/ShopTitansProfitCalculator/img/gold_increase.9d821e97.png"
+  },
+  {
+    "revision": "2c504c4bc0de58b63986dc4647f4e800",
+    "url": "/ShopTitansProfitCalculator/img/herbs.2c504c4b.png"
+  },
+  {
+    "revision": "02a252fe5ba87360c173601aba52191a",
+    "url": "/ShopTitansProfitCalculator/img/instagram.02a252fe.png"
+  },
+  {
+    "revision": "618e2b732c07a8c5a17bd2e6dad76339",
+    "url": "/ShopTitansProfitCalculator/img/iron.618e2b73.png"
+  },
+  {
+    "revision": "1cb03f09bbc4a9b3fb6adb62ceff2a64",
+    "url": "/ShopTitansProfitCalculator/img/ironwood.1cb03f09.png"
+  },
+  {
+    "revision": "893aa5e7492fb61d2b162cb7b81eab16",
+    "url": "/ShopTitansProfitCalculator/img/jewel.893aa5e7.png"
+  },
+  {
+    "revision": "744fe3a68d68428f8164891446ca7a76",
+    "url": "/ShopTitansProfitCalculator/img/leather.744fe3a6.png"
+  },
+  {
+    "revision": "60377b8f2a44d34293e8109b4faa4f9e",
+    "url": "/ShopTitansProfitCalculator/img/linkedin.60377b8f.png"
+  },
+  {
+    "revision": "4e66e49bf51926296904ad598a36a792",
+    "url": "/ShopTitansProfitCalculator/img/mail.4e66e49b.png"
+  },
+  {
+    "revision": "1e1accbb4738664b0483f03f4ae407bf",
+    "url": "/ShopTitansProfitCalculator/img/oil.1e1accbb.png"
+  },
+  {
+    "revision": "d482fca6c36a8b846d628ecff74e2dd3",
+    "url": "/ShopTitansProfitCalculator/img/shoptitanslogo.d482fca6.png"
+  },
+  {
+    "revision": "2ad19daacda785cde3bdf2a271711261",
+    "url": "/ShopTitansProfitCalculator/img/steel.2ad19daa.png"
+  },
+  {
+    "revision": "4bb958e9a4c4de82692b0b145ddd6f75",
+    "url": "/ShopTitansProfitCalculator/img/wood.4bb958e9.png"
+  },
+  {
+    "revision": "2857fc07dd063babed6f438a7d57bfa5",
+    "url": "/ShopTitansProfitCalculator/index.html"
+  },
+  {
+    "revision": "f33f456a7f6b23b7cf27",
+    "url": "/ShopTitansProfitCalculator/js/app.359bfe74.js"
+  },
+  {
+    "revision": "814d40aa4d5fd1b4c348",
+    "url": "/ShopTitansProfitCalculator/js/chunk-vendors.adf0986a.js"
+  },
+  {
+    "revision": "ed640fd4dc7968b0275a50f65fbce492",
+    "url": "/ShopTitansProfitCalculator/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/ShopTitansProfitCalculator/robots.txt"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/ShopTitansProfitCalculator/precache-manifest.9b1e93d8d3bb27924150bd5aa2b8a768.js"
+);
+
+workbox.core.setCacheNameDetails({prefix: "shop-titans-calculator"});
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
